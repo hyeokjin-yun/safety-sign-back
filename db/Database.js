3c62828b96933b9edd55d84f0a85768fe925626f
DESKTOP-41VGKHB\user <rkawk2239@naver.com>
2024-08-19 16:19:38 +0900

    server setting

--- db/Database.js
+++ db/Database.js
@@ -9,5 +9,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 export const db = pool.promise();
