81fe66f3bb4367117535135facb7f56318889ce8
DESKTOP-41VGKHB\user <rkawk2239@naver.com>
2024-08-19 17:54:12 +0900

    서버 응답 확인

--- router/MemberRouter.js
+++ router/MemberRouter.js
@@ -5,6 +5,5 @@ const router = express.Router();
 
 router.post("/signUp", controller.signUp);
 router.post("/login", controller.login);
-router.get("/check", () => console.log("확인"));
 
 export default router;
