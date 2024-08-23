a6085125196d861434083ce833d0bc656aa0ca37
DESKTOP-41VGKHB\user <rkawk2239@naver.com>
2024-08-23 23:26:47 +0900

    fix

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import MemberRouter from "./router/MemberRouter.js";
 import ServiceRouter from "./router/ServiceRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -12,6 +15,6 @@ app.use(cors());
 app.use("/member", MemberRouter);
 app.use("/service", ServiceRouter);
 
-app.listen(443, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("간판지킴이 서버 실행중...");
 });
